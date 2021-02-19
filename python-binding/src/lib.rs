7736cbc9436cd7fed573dd1dedb35cd25db5d54b
Arnaud Taffanel <arnaud@taffanel.org>
2021-02-19 11:18:26 +0100

    Implement minimal python binding allowing to scan and connect
    Error management is non-existent for now and there is no way to close a
    a connection.

--- python-binding/src/lib.rs
+++ python-binding/src/lib.rs
@@ -2,16 +2,61 @@
 use pyo3::prelude::*;
 use pyo3::wrap_pyfunction;
 
-/// Formats the sum of two numbers as string.
-#[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
+#[pyclass]
+struct LinkContext {
+    context: crazyflie_link::LinkContext,
 }
 
-/// A Python module implemented in Rust.
+#[pymethods]
+impl LinkContext {
+    #[new]
+    fn new() -> PyResult<Self> {
+        let context = crazyflie_link::LinkContext::new().unwrap();
+        
+        Ok(LinkContext {
+            context
+        })
+    }
+
+    fn scan(&self) -> PyResult<Vec<String>> {
+        let result = self.context.scan().unwrap();
+
+        Ok(result)
+    }
+
+    fn open_link(&self, uri: &str) -> PyResult<Connection> {
+        let connection = self.context.open_link(uri).unwrap();
+
+        Ok(Connection{connection})
+    }
+}
+
+#[pyclass]
+struct Connection {
+    connection: crazyflie_link::Connection,
+}
+
+#[pymethods]
+impl Connection {
+    fn send_packet(&self, py: Python, packet: Vec<u8>) {
+        py.allow_threads(move || {
+            self.connection.send_packet(packet).unwrap();
+        })
+        
+    }
+
+    fn receive_packet(&self, py: Python) -> PyResult<Vec<u8>> {
+        py.allow_threads(move || {
+            Ok(self.connection.recv_packet().unwrap())
+        })
+    }
+
+    // TODO: Implement closing the connection
+}
+
 #[pymodule]
 fn cflinkrs(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_class::<LinkContext>()?;
 
     Ok(())
 }
