736a28ec5a4febe0ef2a8b4720991ff1d8a6d744
Jonas Danielsson <jonas@bitcraze.io>
2021-03-17 11:16:02 +0100

    radio_thread: [clippy] redundant field names
    https://rust-lang.github.io/rust-clippy/master/index.html#redundant_field_names

--- crazyflie-link/src/radio_thread.rs
+++ crazyflie-link/src/radio_thread.rs
@@ -70,10 +70,10 @@ impl RadioThread {
         self.radio_command
             .send(RadioCommand::Scan {
                 client: self.scan_res_send.clone(),
-                start: start,
-                stop: stop,
-                address: address,
-                payload: payload,
+                start,
+                stop,
+                address,
+                payload,
             })
             .unwrap();
 
@@ -91,9 +91,9 @@ impl RadioThread {
         self.radio_command
             .send(RadioCommand::SendPacket {
                 client: self.send_packet_res_send.clone(),
-                channel: channel,
-                address: address,
-                payload: payload,
+                channel,
+                address,
+                payload,
             })
             .unwrap();
 
